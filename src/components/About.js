bc1fd2d378fa819edfb02eb247452697757f9630
sheready <csstlw@gmail.com>
2022-07-22 12:30:35 +0300

    pass  tests

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && props.bio !== '' && <p>{props.bio}</p> }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={props.github} linkedin={props.linkedin} />
       {/* add your <Links /> component here */}
     </div>
   );
