0cae23dd2d9ef7d4582bc6e92300dc34fdafb742
sheready <csstlw@gmail.com>
2022-07-22 15:43:05 +0300

    pass  tests

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,20 @@ import React from "react";
 import Links from "./Links";
 
 function About(props) {
+
+  // const renderBio = (bio) => {
+  //   if (bio && bio !== '') {
+  //     return <p>{bio}</p>
+  //   }
+  //   return null
+  // }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio && props.bio !== '' && <p>{props.bio}</p> }
+      {/* {renderBio(props.bio)} */}
+      {props.bio && props.bio !== '' ? <p>{props.bio}</p> : null}
+      {/* {props.bio && props.bio !== '' && <p>{props.bio}</p>} */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={props.github} linkedin={props.linkedin} />
       {/* add your <Links /> component here */}
